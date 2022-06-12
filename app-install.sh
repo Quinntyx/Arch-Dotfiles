aa7f0e50c078347181170a2b9290968aff2297f3
Zlare <harrisonhenry916@gmail.com>
2022-06-12 14:29:06 -0700

    Update app-install.sh

--- app-install.sh
+++ app-install.sh
@@ -1,5 +1,5 @@
 echo 'Installing widely-used packages (mlocate, wget, etc.)'
-sudo pacman -S --noconfirm mlocate wget git base base-devel
+sudo pacman -S --noconfirm mlocate wget git base base-devel python3
 
 echo 'Installing messaging clients'
 sudo pacman -S --noconfirm discord-canary
