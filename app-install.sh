a6e633a81faaf083c0954a02e03870d5ecf63eff
Zlare <harrisonhenry916@gmail.com>
2022-06-12 18:24:20 -0700

    oops

--- app-install.sh
+++ app-install.sh
@@ -1,130 +1,134 @@
 alias pinst='sudo pacman -S --noconfirm'
 B='\033[1;34m'
 NOCOLOR='\033[0m'
-alias echo='f() { echo; echo -e "    ${B}$@${NOCOLOR}"; unset -f f; }; f'
+alias fecho='f() { echo -e "    ${B}$@{NOCOLOR}"; unset -f f; } f'
+alias techo='f() { echo; echo -e "    ${B}$@${NOCOLOR}"; unset -f f; }; f'
 echo 'Updating mirrors...'
 sudo pacman -Sy
 
-echo 'Creating src folder'
+techo 'Creating src folder'
 cd ~
 mkdir src
 cd src
 
-echo 'Installing widely-used packages (mlocate, wget, etc.)'
+techo 'Installing widely-used packages (mlocate, wget, etc.)'
 pinst mlocate wget git base base-devel python3 zsh
 
-echo 'Installing messaging clients'
+techo 'Installing messaging clients'
 pinst discord-canary
 
-echo 'Installing Kitty (terminal emulator)'
+techo 'Installing Kitty (terminal emulator)'
 pinst kitty
+
+techo 'Installing Fira Code'
 pinst ttf-fira-code
 
-echo 'Installing theming apps (skip if you have a DE that packages these, like Plasma, LXQt, XFCE, etc.)'
+techo 'Installing theming apps (skip if you have a DE that packages these, like Plasma, LXQt, XFCE, etc.)'
 read -p 'Install? [Y/n] ' response
 
 case $response in [yY][eE][sS]|[yY]|[jJ]|'')
-  echo 'Okay, installing theming'
+  fecho 'Okay, installing theming'
   pinst lxappearance qt5ct-kde
   ;;
-?) echo 'Skipping...';;
+?) fecho 'Skipping...';;
 esac
 
   
-echo 'Installing polybar'
+techo 'Installing polybar'
 pinst polybar
 
-echo 'Installing yay (AUR helper)'
+techo 'Installing yay (AUR helper)'
 pinst yay
 
-echo 'Installing Neovim'
+techo 'Installing Neovim'
 pinst neovim
 
 echo
-echo ' User Apps'
-echo '<--------->'
+techo ' User Apps'
+fecho '<--------->'
 
 read -p 'Install graphics software? (GIMP, Inkscape) [Y/n] ' response
 case $response in [yY][eE][sS]|[yY]|[jJ]|'')
-  echo 'Installing'
+  fecho 'Installing'
   pinst gimp inkscape
   ;;
-?) echo 'Skipping...';;
+?) fecho 'Skipping...';;
 esac
 
 read -p 'Install LibreOffice Suite? [Y/n] ' response
 case $response in [yY][eE][sS]|[yY]|[jJ]|'')
-  echo 'Installing'
+  fecho 'Installing'
   pinst libreoffice-fresh
   ;;
-?) echo 'Skipping...';;
+?) fecho 'Skipping...';;
 esac
 
 read -p 'Install flameshot? [Y/n] ' response
 case $response in [yY][eE][sS]|[yY]|[jJ]|'')
-  echo 'Installing'
+  fecho 'Installing'
   pinst flameshot
   ;;
-?) echo 'Skipping...';;
+?) fecho 'Skipping...';;
 esac
 
 read -p 'Install sxhkd (Simple X HotKey Daemon)? [Y/n] ' response
 case $response in [yY][eE][sS]|[yY]|[jJ]|'')
-  echo 'Installing'
+  fecho 'Installing'
   pinst sxhkd
-  echo 'sxhkd installed, to get it to start on system please put it in the .rc file of your choice. '
+  fecho 'sxhkd installed, to get it to start on system please put it in the .rc file of your choice. '
   read -p 'Start sxhkd? [Y/n] ' response2
   case $response2 in [yY][eE][sS]|[yY]|[jJ]|'')
     sxhkd &> /dev/null & disown
     ;;
-  ?) echo 'Not starting sxhkd. To start manually, run `sxhkd &`';;
+  ?) fecho 'Not starting sxhkd. To start manually, run `sxhkd &`';;
   esac
   ;;
-?) echo 'Skipping...';;
+?) fecho 'Skipping...';;
 esac
 
 read -p 'Install powerlevel10k? [Y/n] ' response
 case $response in [yY][eE][sS]|[yY]|[jJ]|'')
-  echo 'Installing'
+  fecho 'Installing'
   yay -S --noconfirm zsh-theme-powerlevel10k-git
   echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
   ;;
-?) echo 'Skipping...';;
+?) fecho 'Skipping...';;
 esac
 
 read -p 'Install NvChad? [Y/n] ' response
 case $response in [yY][eE][sS]|[yY]|[jJ]|'')
-  echo 'Installing ripgrep (Required for Telescope Searching)'
+  fecho 'Installing ripgrep (Required for Telescope Searching)'
   pinst ripgrep
-  echo 'Installing NvChad...'
+  techo 'Installing NvChad...'
   git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
   ;;
-?) echo 'Skipping...';;
+?) fecho 'Skipping...';;
 esac
 
 read -p 'Install Powercord? [Y/n] ' response
 case $response in [yY][eE][sS]|[yY]|[jJ]|'')
-  echo 'Installing nodejs, npm'
+  fecho 'Installing nodejs, npm'
   pinst nodejs-lts-gallium npm
-  echo 'Installing Powercord...'
+  techo 'Installing Powercord...'
   git clone https://github.com/powercord-org/powercord 
   cd powercord
   npm i
   sudo npm run plug
   cd ..
-  echo 'Restarting Discord Canary'
+  techo 'Restarting Discord Canary'
   killall DiscordCanary 
   discord-canary &> /dev/null & disown
   ;;
 ?) echo 'Skipping...';;
 esac
 
-echo You have reached the end of the app install wizard. 
+techo You have reached the end of the app install wizard. 
 read -p 'Switch to Kitty? [Y/n] ' response
 case $response in [yY][eE][sS]|[yY]|[jJ]|'')
-  echo 'Switching!'
+  fecho 'Switching!'
   kitty & disown
-  kill -9 $PPID
+  sleep 3;
+  # kill -9 $PPID
   ;;
 ?) ;;
 esac
