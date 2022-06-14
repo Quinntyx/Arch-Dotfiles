cfbae7048cd5b95610a7ad7335a4a034c4958b95
Zlare <harrisonhenry916@gmail.com>
2022-06-14 15:55:43 -0700

    Update artix-archlinux-support.sh

--- artix-archlinux-support.sh
+++ artix-archlinux-support.sh
@@ -4,7 +4,7 @@ sudo pacman -S --noconfirm artix-archlinux-support
 
 echo Updating pacman.conf 
 
-sudo mv pacman/pacman.conf /etc/pacman.conf
+sudo cp pacman/pacman.conf /etc/pacman.conf
 
 echo Updating repositories
 sudo pacman-key --populate archlinux
