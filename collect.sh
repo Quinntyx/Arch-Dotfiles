bff32b622cce40ed5f55704d6d738e87ccd5e157
Zlare <harrisonhenry916@gmail.com>
2022-06-13 20:48:59 -0700

    pipewire!

--- collect.sh
+++ collect.sh
@@ -3,9 +3,9 @@ echo "SXHKD: "
 cp ~/.config/sxhkd/sxhkdrc sxhkd/sxhkdrc
 echo "    Done. "
 
-echo "Pacman: "
-cp /etc/pacman.conf pacman/pacman.conf
-echo "    Done. "
+# echo "Pacman: "
+# cp /etc/pacman.conf pacman/pacman.conf
+# echo "    Done. "
 
 echo "Picom: "
 cp ~/.config/picom/picom.conf picom/picom.conf
@@ -17,4 +17,8 @@ echo "    Done. "
 
 echo "BSPWM: "
 cp ~/.config/bspwm/bspwmrc bspwm/bspwmrc
+echo "    Done. "
+
+echo "Pipewire: "
+cp /etc/pipewire/pipewire.conf conf/pipewire.conf
 echo "    Done. "
