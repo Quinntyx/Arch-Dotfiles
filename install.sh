fd80e4ef390f82c802cf7030cdbddc7610167cdc
Zlare <harrisonhenry916@gmail.com>
2022-08-07 21:21:42 -0700

    Updated installer

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
-./artix-archlinux-support.sh
+read -p "Are you running Artix (no Systemd)? [y/N]: " response
+
+
+case $response in [yY][eE][sS]|[yY]|[jJ]|'')
+   echo "Okay, installing artix-archlinux-support"
+   ./artix-archlinux-support.sh
+   ;;
+?)
+   echo "Skipping artix-archlinux-support"
+   ;;
+esac
+
 ./kitty.sh
 ./app-install.sh
 ./config.sh
